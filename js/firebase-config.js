801ed15d725fe394dcae920d9ff737f58dd6f5ed
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-04-07 11:40:51 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,12 @@
-// استدعاء مكتبات فايربيز الأساسية
+// 1. هنا بنستدعي (Import) الأدوات اللي محتاجينها من شركة جوجل
+// initializeApp: دي الأداة اللي بتصحي المشروع وتوقفه على رجله
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// getAuth: دي الأداة المسئولة عن تسجيل الدخول والباسوردات
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// getDatabase: دي الأداة المسئولة عن حفظ وقراية البيانات (المنتجات، الطلبات)
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// إعدادات مشروعك
+// 2. دي "البطاقة الشخصية" بتاعة مشروعك اللي إنت لسه باعتها
 const firebaseConfig = {
   apiKey: "AIzaSyCyypXt8LnHiVBR2Ka0Mb8ntXu6dJH9N-w",
   authDomain: "start-online-6f460.firebaseapp.com",
@@ -14,7 +17,8 @@ const firebaseConfig = {
   appId: "1:343437561451:web:2318fb3b90eb48fd0a6b2e"
 };
 
-// تهيئة الاتصال وتصديره لكل الصفحات
-export const app = initializeApp(firebaseConfig); 
-export const auth = getAuth(app); 
-export const db = getDatabase(app);
+// 3. هنا بقى السحر (export)
+// كلمة export معناها "صدّر أو طلّع بره". يعني بنجهز الأدوات دي عشان أي صفحة تانية في الموقع تاخدها على الجاهز
+export const app = initializeApp(firebaseConfig); // شغلنا المشروع
+export const auth = getAuth(app); // جهزنا نظام الدخول
+export const db = getDatabase(app); // جهزنا قاعدة البيانات
